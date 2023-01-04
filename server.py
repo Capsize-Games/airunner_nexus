e8f394d7f6f7a5c44d0269d8cffd9376b4d89b8b
w4ffl35 (Joe Curlee) <25737761+w4ffl35@users.noreply.github.com>
2023-01-04 16:28:25 -0700

    Fixes switching between v1 ckpt models and diffusers + v2

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def parse_args():
     parser.add_argument('--port', type=int, default=5000)
     parser.add_argument('--host', type=str, default='http://localhost')
     parser.add_argument('--timeout', action='store_true', default=False)
-    parser.add_argument('--chunk-size', type=int, default=1)
+    parser.add_argument('--chunk-size', type=int, default=1024)
     parser.add_argument('--max-client-connections', type=int, default=1)
     parser.add_argument('--model-base-path', type=str, default='.')
 
