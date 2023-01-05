9a3443c2ed5978575b1371619fba4b560f6c3468
w4ffl35 <w4ffl35@ml1.net>
2023-01-04 20:12:49 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,6 @@ export DISABLE_TELEMETRY=true
 export HF_ENDPOINT=""
 export HF_HUB_OFFLINE=true
 
-
-# add /home/joe/Projects/ai/runai2/diffusers to python include path
 export KRITA_STABLE_DIFFUSION_CKPT_DUMP_PATH=diffusers_stable_diffusion_ckpts
 
 /home/joe/miniconda3/envs/runai2/bin/python server.py
