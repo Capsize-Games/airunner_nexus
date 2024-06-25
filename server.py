f7d5ca9dc00a12518a9866e875e267c5e6b5abd0
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-06-25 07:03:01 -0600

    Ability to run LLM or SD server

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 import argparse
 import settings
-
+from llm_request_queue_worker import LLMRequestQueueWorker
 from stable_diffusion_request_queue_worker import StableDiffusionRequestQueueWorker
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--server', type=int, default=settings.DEFAULT_SERVER_TYPE)
     parser.add_argument('--port', type=int, default=settings.DEFAULT_PORT)
     parser.add_argument('--host', type=str, default=settings.DEFAULT_HOST)
     parser.add_argument('--timeout', action='store_true', default=False)
@@ -18,7 +20,13 @@ def parse_args():
 if __name__ == '__main__':
     # get command line arguments
     args = parse_args()
-    app = StableDiffusionRequestQueueWorker(
+    if args.server == "LLM":
+        server_class_ = LLMRequestQueueWorker
+    elif args.server == "SD":
+        server_class_ = StableDiffusionRequestQueueWorker
+    else:
+        raise ValueError(f"Unknown server type: {args.server}")
+    app = server_class_(
         port=args.port,
         host=args.host,
         do_timeout=args.timeout,
--- settings.py
+++ settings.py
@@ -6,14 +6,7 @@ DEFAULT_PORT = 50006
 DEFAULT_HOST = "0.0.0.0"
 MAX_CLIENTS = 1
 DEBUG = True
+DEFAULT_SERVER_TYPE = "LLM"
 
 # Settings for auto updates
-KSD_VERSION_CHECK_URL = "https://raw.githubusercontent.com/w4ffl35/krita_stable_diffusion/master/VERSION"
-RUNAI_VERSION_CHECK_URL = "https://raw.githubusercontent.com/w4ffl35/run-ai-socket-server/master/VERSION"
 CACHE_TIME = 60 * 10  # 10 minutes
-KSD_VERSION_FILE = "ksd_version.txt"
-RUNAI_VERSION_FILE = "runai_version.txt"
-BACKUP_DIR = os.path.join("tmp", "runai")
-UPGRADE_PATH = os.path.join("tmp", "upgrade")
-KSD_DOWNLOAD_FILE = os.path.join(UPGRADE_PATH, "ksd.latest.tar.gz")
-RUNAI_DOWNLOAD_FILE = os.path.join(UPGRADE_PATH, "runai.latest.tar.gz")
