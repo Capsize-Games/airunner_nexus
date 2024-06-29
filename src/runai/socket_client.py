e96229215c8efbc46774e8b3f8e55cb441fd256a
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-06-29 17:45:18 -0600

    handle errors in socket client

--- src/runai/socket_client.py
+++ src/runai/socket_client.py
@@ -10,7 +10,10 @@ class SocketClient:
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect(self):
-        self.client_socket.connect((self.host, self.port))
+        try:
+            self.client_socket.connect((self.host, self.port))
+        except ConnectionRefusedError:
+            print("Connection refused. Make sure the server is running.")
 
     def send_message(self, message):
         message = message.encode('utf-8')  # encode the message as UTF-8
@@ -19,17 +22,28 @@ class SocketClient:
             message = message[self.packet_size:]
             if len(packet) < self.packet_size:
                 packet += b'\x00' * (self.packet_size - len(packet))  # pad the message with null bytes
-            self.client_socket.sendall(packet)
+            try:
+                self.client_socket.sendall(packet)
+            except BrokenPipeError:
+                print("Connection lost. Make sure the server is running.")
+                break
         self.send_end_message()
 
     def send_end_message(self):
         # send a message of all zeroes of expected_byte_size length
         # to indicate that the image is being sent
-        self.client_socket.sendall(b'\x00' * self.packet_size)
+        try:
+            self.client_socket.sendall(b'\x00' * self.packet_size)
+        except BrokenPipeError:
+            print("Connection lost. Make sure the server is running.")
 
     def receive_message(self):
         while True:
-            packet = self.client_socket.recv(self.packet_size)
+            try:
+                packet = self.client_socket.recv(self.packet_size)
+            except OSError:
+                print("Connection lost. Make sure the server is running.")
+                break
             if packet == b'\x00' * self.packet_size:  # end message received
                 break
             yield packet.decode('utf-8')  # decode the packet as UTF-8
