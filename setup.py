c53bfc23f46cdc9e831964f8a659d5ecdb6a47d4
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-08-15 08:15:30 -0600

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="airunner_nexus",
-    version="v1.4.6",
+    version="v1.5.0",
     author="Capsize LLC",
     description="Run a socket server for AI models.",
     long_description=open("README.md", "r", encoding="utf-8").read(),
